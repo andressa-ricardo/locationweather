7b5ebcb5496f9e157d2f675c7eabec6fd79187b0
andressa-ricardo <andressaricardo30@gmail.com>
2025-03-18 11:05:32 -0300

    fix: add class-validator

--- src/weather/weather.dto.ts
+++ src/weather/weather.dto.ts
@@ -1,21 +1,29 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class WeatherDto {
   @ApiProperty({ example: 'Rio de Janeiro', description: 'Nome da cidade' })
+  @IsString()
+  @IsNotEmpty()
   cidade: string;
 
   @ApiProperty({ example: 'BR', description: 'Código do país' })
+  @IsString()
   pais: string;
 
   @ApiProperty({ example: '25 °C', description: 'Temperatura atual' })
+  @IsString()
   temperatura: string;
 
   @ApiProperty({ example: 'nublado', description: 'Descrição do clima' })
+  @IsString()
   descricao: string;
 
   @ApiProperty({ example: '60%', description: 'Umidade do ar' })
+  @IsString()
   umidade: string;
 
   @ApiProperty({ example: '5 m/s', description: 'Velocidade do vento' })
+  @IsString()
   vento: string;
 }
